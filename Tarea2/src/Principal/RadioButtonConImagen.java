00262f173bafe5b1f44b3ea50dcfea9b96cd900b
Alex Blanchard <alexblancor05@gmail.com>
2023-05-19 13:16:42 -0400

    Se añadio la clase RadioButtonConImagen

---
+++ Tarea2/src/Principal/RadioButtonConImagen.java
@@ -0,0 +1,33 @@
+package Principal;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.event.ChangeListener;
+
+public class RadioButtonConImagen extends JPanel {
+    private JRadioButton boton = new JRadioButton();
+    private JLabel imagen;
+    private Border marco = BorderFactory.createLineBorder(Color.black);
+
+    public RadioButtonConImagen(String path, int ancho, int largo){
+        ImageIcon Icon = new ImageIcon(new ImageIcon(path).getImage().getScaledInstance(ancho, largo, Image.SCALE_DEFAULT));
+        imagen = new JLabel(Icon);
+        add(boton);
+        add(imagen);
+    }
+
+    public void addButtonGroup(ButtonGroup grupo){
+        grupo.add(boton);
+    }
+
+    public void addActionListener(ActionListener listener) {
+        boton.addActionListener(listener);
+    }
+
+    public boolean isSelected(){
+        return boton.isSelected();
+    }
+}
