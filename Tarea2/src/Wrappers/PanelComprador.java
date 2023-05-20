524e9ec739b20c31be8b349e82c01319ac6d48fe
and265 <131011527+and265@users.noreply.github.com>
2023-05-20 15:26:33 -0400

    Clase PanelComprador en desarrollo

--- Tarea2/src/Wrappers/PanelComprador.java
+++ Tarea2/src/Wrappers/PanelComprador.java
@@ -7,12 +7,18 @@ public class PanelComprador extends JPanel{
 
     public PanelComprador(){
 
-        this.setSize(350,400);
-        this.setBackground(Color.white);
+        //this.setLayout(null);
+        this.setBounds(370,0,490,400);
+        this.setBackground(Color.green);
 
     }
 
     public void paint (Graphics g){
+
+        g.setColor(Color.black);
+        g.drawRect(450,80,490,400);
+        g.setColor(Color.GREEN);
+        g.fillRect(451,81,488,398);
 
     }
 }
